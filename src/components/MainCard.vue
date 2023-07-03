94486bd74ec83beb250ea334dbcd4f6b30c0b374
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-03 16:33:53 +0200

    set dinamic class

--- src/components/MainCard.vue
+++ src/components/MainCard.vue
@@ -9,7 +9,7 @@ export default {
 </script>
 
 <template>
-    <div class="card">
+    <div class="card" :class="type1">
         <img :src="imageUrl" alt="">
         <h4>{{ number }}</h4>
         <h3>{{ name }}</h3>
@@ -20,10 +20,29 @@ export default {
 <style lang="scss" scoped>
 .card {
     text-align: center;
+    padding: 20px;
+    margin: 10px;
+    border-radius: 30px;
+    box-shadow: 0 0 10px black;
 
     img {
-        display: block;
-        max-width: 100%;
+        width: 150px;
+        height: 150px;
+        border-radius: 50%;
+
     }
+
+}
+
+.Fire {
+    background-color: red;
+}
+
+.Water {
+    background-color: lightskyblue;
+}
+
+.Grass {
+    background-color: lightgreen;
 }
 </style>
