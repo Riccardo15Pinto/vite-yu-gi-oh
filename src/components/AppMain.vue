5b5f39f24660e7afb52321d67d2d4879a2e0c08f
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-03 15:37:19 +0200

    add script and import main card

---
+++ src/components/AppMain.vue
@@ -0,0 +1,13 @@
+<script>
+import MainCard from './MainCard.vue';
+export default {
+    components: { MainCard },
+
+}
+</script>
+
+<template>
+    <MainCard />
+</template>
+
+<style lang="scss" scoped></style>
