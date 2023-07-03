080ad80f99e9a10b2e38f7f189b8fe49511c4e17
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-03 15:37:49 +0200

    add script and import store

---
+++ src/components/MainCard.vue
@@ -0,0 +1,16 @@
+<script>
+import { store } from '../data/data';
+export default {
+    data() {
+        return {
+            store,
+        }
+    }
+}
+</script>
+
+<template>
+    <h1 v-for="pokemon in store.pokemons">{{ pokemon.name }}</h1>
+</template>
+
+<style lang="scss" scoped></style>
