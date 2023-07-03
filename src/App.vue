84c2b6ff94f6b35ad5b8eebcbbf14f1e3a73936f
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-03 15:00:38 +0200

    set app

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <script>
+import axios from '../node_modules/axios';
+import { store } from './data/data';
+const endpoint = "https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons";
 export default {
   data() {
     return {
-
+      store,
     }
+  },
+  methods: {
+    getItemsList(target) {
+      axios.get(target).then(res => {
+        this.store.pokemons = res.data.docs;
+      })
+    }
+  },
+  computed() {
+    this.getItemsList(endpoint)
   }
 }
 </script>
