998cdcbae21cc8e61c89913abef8caee9040f4b3
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-03 16:05:06 +0200

    add main card and set it

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,34 @@
 <script>
+import { store } from '../data/data';
 import MainCard from './MainCard.vue';
 export default {
+    data() {
+        return {
+            store,
+        }
+    },
     components: { MainCard },
 
 }
 </script>
 
 <template>
-    <MainCard />
+    <div class="container">
+        <MainCard v-for="pokemon in store.pokemons" :name="pokemon.name" :number="pokemon.number"
+            :imageUrl="pokemon.imageUrl" :type1="pokemon.type1" />
+
+    </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.container {
+    max-width: 1400px;
+    display: flex;
+    flex-wrap: wrap;
+
+    .card {
+        flex-basis: calc(100% / 4);
+    }
+}
+</style>
+
