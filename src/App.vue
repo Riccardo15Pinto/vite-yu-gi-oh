3f18d77aeb005d6079b477bd8e42023494614346
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-04 13:43:52 +0200

    create getItemsTypeList

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import axios from '../node_modules/axios';
 import { store } from './data/data';
 const endpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons';
+const typeEndpoint = 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons/types1';
 import Appmain from './components/AppMain.vue'
 export default {
   components: { Appmain },
@@ -14,12 +15,19 @@ export default {
     getItemsList(target) {
       axios.get(target).then(res => {
         this.store.pokemons = res.data.docs;
-        console.log(this.store.pokemons)
+      })
+    },
+
+    getItemsTypeList(target) {
+      axios.get(target).then(res => {
+        this.store.mainType = res.data;
       })
     }
   },
   created() {
     this.getItemsList(endpoint)
+
+    this.getItemsTypeList(typeEndpoint)
   }
 }
 </script>
