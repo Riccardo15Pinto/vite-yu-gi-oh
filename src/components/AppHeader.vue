beeeddb6b9389f7a69e408813cea9e37a741bedb
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-04 17:02:19 +0200

    renamed

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+import { store } from '../data/data'
+export default {
+    data() {
+        return {
+            store,
+            selectedOption: '',
+        }
+    },
+    emits: ['select-Option']
+}
+</script>
+
+<template>
+    <select v-model="selectedOption" @change="$emit('select-Option', selectedOption)">
+        <option value="0">Rimuovi filtro/i</option>
+        <option v-for="type in store.mainType" :value="type">{{ type }}
+        </option>
+    </select>
+</template>
+
+<style lang="scss" scoped></style>
