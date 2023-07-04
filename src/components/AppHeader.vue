ce8de3abdc7f04bfec3093599e3857376ddeb682
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-04 19:55:34 +0200

    set line 18, 10

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,16 +7,30 @@ export default {
             selectedOption: '',
         }
     },
-    emits: ['select-Option']
+    props: {
+        isNextDisabled: 'Boolean',
+        isPrevDisabled: 'Boolean',
+    }
 }
 </script>
 
 <template>
-    <select v-model="selectedOption" @change="$emit('select-Option', selectedOption)">
-        <option value="0">Rimuovi filtro/i</option>
-        <option v-for="type in store.mainType" :value="type">{{ type }}
-        </option>
-    </select>
+    <header>
+        <div class="prev" v-if="isPrevDisabled" @click="$emit('prev-click')">indietro</div>
+        <select v-model="selectedOption" @change="$emit('select-Option', selectedOption)">
+            <option value="0">Rimuovi filtro/i</option>
+            <option v-for="type in store.mainType" :value="type">{{ type }}
+            </option>
+        </select>
+        <div class="next" v-if="isNextDisabled" @click="$emit('next-click')">avanti</div>
+    </header>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+
+    select {
+        width: 200px;
+    }
+}
+</style>
