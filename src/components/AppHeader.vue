7b27f9d8e19263ef3c6dad0fc1c99d3c40c79e88
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-04 20:04:00 +0200

    delete props

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,22 +7,18 @@ export default {
             selectedOption: '',
         }
     },
-    props: {
-        isNextDisabled: 'Boolean',
-        isPrevDisabled: 'Boolean',
-    }
 }
 </script>
 
 <template>
     <header>
-        <div class="prev" v-if="isPrevDisabled" @click="$emit('prev-click')">indietro</div>
+        <div class="prev" @click="$emit('prev-click')">indietro</div>
         <select v-model="selectedOption" @change="$emit('select-Option', selectedOption)">
             <option value="0">Rimuovi filtro/i</option>
             <option v-for="type in store.mainType" :value="type">{{ type }}
             </option>
         </select>
-        <div class="next" v-if="isNextDisabled" @click="$emit('next-click')">avanti</div>
+        <div class="next" @click="$emit('next-click')">avanti</div>
     </header>
 </template>
 
