1d0d91a48697e7a006f826f44deb4edcd1ab514b
Riccardo15Pinto <riccardopinto59@gmail.com>
2023-07-04 14:38:45 +0200

    structuring mainselect

---
+++ src/components/MainSelect.vue
@@ -0,0 +1,21 @@
+<script>
+import { store } from '../data/data'
+export default {
+    data() {
+        return {
+            store,
+            selectedOption: null,
+        }
+    },
+    emits: ['select-Option']
+}
+</script>
+
+<template>
+    <select v-model="selectedOption">
+        <option value="0">Tipo</option>
+        <option v-for="type in store.mainType" :value="type" @click="$emit('select-Option')">{{ type }}</option>
+    </select>
+</template>
+
+<style lang="scss" scoped></style>
